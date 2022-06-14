e42305133a7fb9cd6549644c1a20451b7e727eed
Othmane Mezgueldi <othmane.mezgueldi@gmail.com>
2022-06-14 21:25:32 +0000

    master page

---
+++ index.php
@@ -0,0 +1,21 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <?php require_once "pages/body/head.php"; ?>
+    <?php require_once "pages/body/script.php"; ?>
+
+</head>
+
+<body>
+    <?php require_once "pages/body/nav.php"; ?>
+
+    <div class="container mt-5">
+
+    </div>
+
+
+    <?php require_once "pages/body/footer.php"; ?>
+</body>
+
+</html>
---
+++ pages/body/head.php
@@ -0,0 +1,7 @@
+<title>Title</title>
+<!-- Required meta tags -->
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+<!-- Bootstrap CSS v5.2.0-beta1 -->
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
