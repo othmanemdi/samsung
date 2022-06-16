15509ee1cd8c7416a89f4a1b889dc500054e3f7e
Othmane Mezgueldi <othmane.mezgueldi@gmail.com>
2022-06-16 21:28:03 +0000

    Creat contact page

---
+++ pages/contact.php
@@ -0,0 +1,30 @@
+<?php
+
+ob_start();
+
+$title = "Contact page";
+
+$content_php = ob_get_clean();
+
+ob_start(); ?>
+<style>
+
+</style>
+
+<?php $content_css = ob_get_clean();
+
+ob_start(); ?>
+
+<h1>Contact</h1>
+
+
+
+<?php $content_html = ob_get_clean();
+
+ob_start(); ?>
+
+<script>
+    // alert(123)
+</script>
+
+<?php $content_js = ob_get_clean(); ?>
