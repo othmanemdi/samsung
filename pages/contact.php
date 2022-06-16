8bc5b49a37aec300931bb9be5041c83959978473
Othmane Mezgueldi <othmane.mezgueldi@gmail.com>
2022-06-16 21:30:39 +0000

    Add master page to contact page

--- pages/contact.php
+++ pages/contact.php
@@ -2,7 +2,7 @@
 
 ob_start();
 
-$title = "Contact page";
+$title = "Contact page page";
 
 $content_php = ob_get_clean();
 
@@ -15,7 +15,7 @@ ob_start(); ?>
 
 ob_start(); ?>
 
-<h1>Contact</h1>
+<h1>Contact page</h1>
 
 
 
