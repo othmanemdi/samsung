d11bb97b7285083cf5a481f510f6445e7e9e469e
Othmane Mezgueldi <othmane.mezgueldi@gmail.com>
2022-06-21 21:03:20 +0000

    Addung samsung product

--- pages/shop.php
+++ pages/shop.php
@@ -60,235 +60,25 @@ ob_start(); ?>
 
         <!-- Catalogue -->
         <div class="row">
+            <?php for ($i = 1; $i <= 8; $i++) : ?>
+                <div class="col-lg-4 col-md-6 col-sm-6">
 
-            <div class="col-lg-4 col-md-6 col-sm-6">
-
-                <div class="card mb-3">
-                    <a href="product_details">
-                        <img height="300" src="images/produits/0.jpg" class="card-img-top" alt="...">
-                    </a>
-                    <div class="card-body">
-                        <h5 class="card-title">Produit 1</h5>
-                        <div class="mb-3">
-                            <b>4 500.00 DH</b> <del class="text-danger fw-bold">5 000.00 DH</del>
+                    <div class="card mb-3">
+                        <a href="product_details">
+                            <img height="300" src="images/products/p<?= $i ?>.jpg" class="card-img-top" alt="...">
+                        </a>
+                        <div class="card-body">
+                            <h5 class="card-title">Produit <?= $i ?>.</h5>
+                            <div class="mb-3">
+                                <b>4 500.00 DH</b> <del class="text-danger fw-bold">5 000.00 DH</del>
+                            </div>
+                            <a href="cart" class="btn btn-dark">Ajouter au panier</a>
                         </div>
-                        <a href="cart" class="btn btn-dark">Ajouter au panier</a>
                     </div>
                 </div>
-            </div>
-            <!-- col -->
+                <!-- col -->
+            <?php endfor ?>
 
-            <div class="col-lg-4 col-md-6 col-sm-6">
-
-                <div class="card mb-3">
-                    <a href="product_details">
-                        <img height="300" src="images/produits/11.jpg" class="card-img-top" alt="...">
-                    </a>
-                    <div class="card-body">
-                        <h5 class="card-title">Produit 1</h5>
-                        <div class="mb-3">
-                            <b>4 500.00 DH</b> <del class="text-danger fw-bold">5 000.00 DH</del>
-                        </div>
-                        <a href="cart" class="btn btn-dark">Ajouter au panier</a>
-                    </div>
-                </div>
-            </div>
-            <!-- col -->
-
-            <div class="col-lg-4 col-md-6 col-sm-6">
-
-                <div class="card mb-3">
-                    <a href="product_details">
-                        <img height="300" src="images/produits/12.jpg" class="card-img-top" alt="...">
-                    </a>
-                    <div class="card-body">
-                        <h5 class="card-title">Produit 1</h5>
-                        <div class="mb-3">
-                            <b>4 500.00 DH</b> <del class="text-danger fw-bold">5 000.00 DH</del>
-                        </div>
-                        <a href="cart" class="btn btn-dark">Ajouter au panier</a>
-                    </div>
-                </div>
-            </div>
-            <!-- col -->
-
-
-            <div class="col-lg-4 col-md-6 col-sm-6">
-
-                <div class="card mb-3">
-                    <a href="product_details">
-                        <img height="300" src="images/produits/13.jpg" class="card-img-top" alt="...">
-                    </a>
-                    <div class="card-body">
-                        <h5 class="card-title">Produit 1</h5>
-                        <div class="mb-3">
-                            <b>4 500.00 DH</b> <del class="text-danger fw-bold">5 000.00 DH</del>
-                        </div>
-                        <a href="cart" class="btn btn-dark">Ajouter au panier</a>
-                    </div>
-                </div>
-            </div>
-            <!-- col -->
-
-
-            <div class="col-lg-4 col-md-6 col-sm-6">
-
-                <div class="card mb-3">
-                    <a href="product_details">
-                        <img height="300" src="images/produits/14.jpg" class="card-img-top" alt="...">
-                    </a>
-                    <div class="card-body">
-                        <h5 class="card-title">Produit 1</h5>
-                        <div class="mb-3">
-                            <b>4 500.00 DH</b> <del class="text-danger fw-bold">5 000.00 DH</del>
-                        </div>
-                        <a href="cart" class="btn btn-dark">Ajouter au panier</a>
-                    </div>
-                </div>
-            </div>
-            <!-- col -->
-
-
-            <div class="col-lg-4 col-md-6 col-sm-6">
-
-                <div class="card mb-3">
-                    <a href="product_details">
-                        <img height="300" src="images/produits/1.jpg" class="card-img-top" alt="...">
-                    </a>
-                    <div class="card-body">
-                        <h5 class="card-title">Produit 1</h5>
-                        <div class="mb-3">
-                            <b>4 500.00 DH</b> <del class="text-danger fw-bold">5 000.00 DH</del>
-                        </div>
-                        <a href="cart" class="btn btn-dark">Ajouter au panier</a>
-                    </div>
-                </div>
-            </div>
-            <!-- col -->
-
-
-            <div class="col-lg-4 col-md-6 col-sm-6">
-                <div class="card mb-3">
-                    <a href="product_details">
-
-                        <img height="300" src="images/produits/2.jpg" class="card-img-top" alt="...">
-                    </a>
-                    <div class="card-body">
-                        <h5 class="card-title">A 35 5G</h5>
-                        <div class="mb-3">
-                            <b>4 500.00 DH</b> <del class="text-danger fw-bold">5 000.00 DH</del>
-                        </div>
-                        <a href="cart" class="btn btn-dark">Ajouter au panier</a>
-                    </div>
-                </div>
-            </div>
-            <!-- col -->
-
-
-            <div class="col-lg-4 col-md-6 col-sm-6">
-                <div class="card mb-3">
-                    <a href="product_details">
-
-                        <img height="300" src="images/produits/3.jpg" class="card-img-top" alt="...">
-                    </a>
-                    <div class="card-body">
-                        <h5 class="card-title">A 35 5G</h5>
-                        <div class="mb-3">
-                            <b>4 500.00 DH</b> <del class="text-danger fw-bold">5 000.00 DH</del>
-                        </div>
-                        <a href="cart" class="btn btn-dark">Ajouter au panier</a>
-                    </div>
-                </div>
-            </div>
-            <!-- col -->
-
-
-
-            <div class="col-lg-4 col-md-6 col-sm-6">
-                <div class="card mb-3">
-                    <a href="product_details">
-
-                        <img height="300" src="images/produits/4.jpg" class="card-img-top" alt="...">
-                    </a>
-                    <div class="card-body">
-                        <h5 class="card-title">A 35 5G</h5>
-                        <div class="mb-3">
-                            <b>4 500.00 DH</b> <del class="text-danger fw-bold">5 000.00 DH</del>
-                        </div>
-                        <a href="cart" class="btn btn-dark">Ajouter au panier</a>
-                    </div>
-                </div>
-            </div>
-            <!-- col -->
-
-            <div class="col-lg-4 col-md-6 col-sm-6">
-                <div class="card mb-3">
-                    <a href="product_details">
-
-                        <img height="300" src="images/produits/5.jpg" class="card-img-top" alt="...">
-                    </a>
-                    <div class="card-body">
-                        <h5 class="card-title">A 35 5G</h5>
-                        <div class="mb-3">
-                            <b>4 500.00 DH</b> <del class="text-danger fw-bold">5 000.00 DH</del>
-                        </div>
-                        <a href="cart" class="btn btn-dark">Ajouter au panier</a>
-                    </div>
-                </div>
-            </div>
-            <!-- col -->
-
-
-            <div class="col-lg-4 col-md-6 col-sm-6">
-                <div class="card mb-3">
-                    <a href="product_details">
-
-                        <img height="300" src="images/produits/6.jpg" class="card-img-top" alt="...">
-                    </a>
-                    <div class="card-body">
-                        <h5 class="card-title">A 35 5G</h5>
-                        <div class="mb-3">
-                            <b>4 500.00 DH</b> <del class="text-danger fw-bold">5 000.00 DH</del>
-                        </div>
-                        <a href="cart" class="btn btn-dark">Ajouter au panier</a>
-                    </div>
-                </div>
-            </div>
-            <!-- col -->
-
-            <div class="col-lg-4 col-md-6 col-sm-6">
-                <div class="card mb-3">
-                    <a href="product_details">
-
-                        <img height="300" src="images/produits/7.jpg" class="card-img-top" alt="...">
-                    </a>
-                    <div class="card-body">
-                        <h5 class="card-title">A 35 5G</h5>
-                        <div class="mb-3">
-                            <b>4 500.00 DH</b> <del class="text-danger fw-bold">5 000.00 DH</del>
-                        </div>
-                        <a href="cart" class="btn btn-dark">Ajouter au panier</a>
-                    </div>
-                </div>
-            </div>
-            <!-- col -->
-
-            <div class="col-lg-4 col-md-6 col-sm-6">
-                <div class="card mb-3">
-                    <a href="product_details">
-
-                        <img height="300" src="images/produits/8.jpg" class="card-img-top" alt="...">
-                    </a>
-                    <div class="card-body">
-                        <h5 class="card-title">A 35 5G</h5>
-                        <div class="mb-3">
-                            <b>4 500.00 DH</b> <del class="text-danger fw-bold">5 000.00 DH</del>
-                        </div>
-                        <a href="cart" class="btn btn-dark">Ajouter au panier</a>
-                    </div>
-                </div>
-            </div>
-            <!-- col -->
         </div>
         <!-- fin row catalogue-->
 
