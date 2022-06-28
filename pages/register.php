f838e2d67d4e9df0856cc3ad64733603eebacc04
Othmane Mezgueldi <othmane.mezgueldi@gmail.com>
2022-06-28 21:25:30 +0000

    Check all errors from register page

--- pages/register.php
+++ pages/register.php
@@ -4,6 +4,88 @@ ob_start();
 
 $title = "Register page";
 
+$errors = [];
+
+if (isset($_POST['register_btn'])) {
+
+    $prenom = $_POST['prenom'];
+    $nom = $_POST['nom'];
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+    $password_confirme = $_POST['password_confirme'];
+
+    if (empty($prenom) or !preg_match('/^[a-zA-Z]+$/', $prenom) or strlen($prenom) < 3) {
+        // $errors["prenom"] = "Votre prénome n'est pas valide";
+        $errors["prenom"] = "";
+        if (empty($prenom)) {
+            $errors["prenom"] .= "Veuillez saisir votre prenom SVP ";
+        } else {
+
+            if (!preg_match('/^[a-zA-Z]+$/', $prenom)) {
+                $errors["prenom"] .= "Veuillez entrer des caractères alphabétique ";
+            }
+            if (strlen($prenom) < 3) {
+                $errors["prenom"] .= "Veuillez entrer plus de 3 caractères ";
+            }
+        }
+        $prenom_class_input = "is-invalid";
+        $prenom_class_feedback = "invalid-feedback";
+    } else {
+        $prenom_class_input = "is-valid";
+        $prenom_class_feedback = "valid-feedback";
+    }
+
+
+
+    if (empty($_POST['nom']) || !preg_match('/^[a-zA-Z]+$/', $_POST['nom'])) {
+        $errors["nom"] = "Votre nom n'est pas valide";
+        $nom_class_input = "is-invalid";
+        $nom_class_feedback = "invalid-feedback";
+    } else {
+        $nom_class_input = "is-valid";
+        $nom_class_feedback = "valid-feedback";
+    }
+
+    if (empty($_POST['email']) || !filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
+        $errors['email'] = "Votre email n'est pas valide";
+        $email_class_input = "is-invalid";
+        $email_class_feedback = "invalid-feedback";
+    } else {
+        $email_class_input = "is-valid";
+        $email_class_feedback = "valid-feedback";
+    }
+
+
+    if (empty($_POST['password']) || !preg_match('/^[a-zA-Z0-9_]+$/', $_POST['password'])) {
+        $errors["password"] = "Votre password n'est pas valide";
+        $password_class_input = "is-invalid";
+        $password_class_feedback = "invalid-feedback";
+    } else {
+        $password_class_input = "is-valid";
+        $password_class_feedback = "valid-feedback";
+    }
+
+    if (empty($_POST['password_confirm']) || ($_POST['password'] != $_POST['password_confirm'])) {
+        $errors["password_confirm"] = "Les deux mots de passe ne sont pas identiques";
+        $password_confirm_class_input = "is-invalid";
+        $password_confirm_class_feedback = "invalid-feedback";
+    } else {
+        $password_confirm_class_input = "is-valid";
+        $password_confirm_class_feedback = "valid-feedback";
+    }
+
+
+
+    if (empty($errors)) {
+    }
+
+
+    // echo "<pre>";
+    // print_r($_POST);
+    // echo "</pre>";
+    // die();
+}
+
 $content_php = ob_get_clean();
 
 ob_start(); ?>
@@ -30,53 +112,68 @@ ob_start(); ?>
 
             <form method="post">
 
-
                 <div class="mb-3">
-                    <label for="prenom" class="form-label">
+
+                    <label class="form-label" for="prenom">
                         Prénom:
                         (<span class="text-danger">*</span>)
                     </label>
-                    <input type="text" class="form-control" name="prenom" placeholder="Votre prénom SVP:">
+
+                    <input name="prenom" type="text" class="form-control 
+                     <?= $prenom_class_input ?? "" ?>
+                     " id="prenom" placeholder="Veuillez saisir votre prénom SVP !" value="<?= $_POST['prenom'] ?? "" ?>">
+
+                    <div class="<?= $prenom_class_feedback ?? "" ?> fw-bold">
+                        <?= $errors['prenom'] ?? "" ?>
+                    </div>
+
                 </div>
 
 
                 <div class="mb-3">
-                    <label for="nom" class="form-label">
-                        Nom:
-                        (<span class="text-danger">*</span>)
-                    </label>
-                    <input type="text" class="form-control" name="nom" placeholder="Votre nom SVP:">
+                    <label class="form-label" for="nom">Nom:</label>
+
+                    <input name="nom" type="text" class="form-control <?= $nom_class_input ?? "" ?>" id="nom" placeholder="Veuillez saisir votre nom SVP !" value="<?= $_POST['nom'] ?? "" ?>">
+
+                    <div class="<?= $nom_class_feedback ?? "" ?> fw-bold">
+                        <?= $errors['nom'] ?? "" ?>
+                    </div>
                 </div>
 
+                <div class="form-group mb-3">
+                    <label class="form-label" for="email">Adresse mail:</label>
 
-                <div class=" mb-3">
-                    <label for="email" class="form-label">
-                        Adresse mail:
-                        (<span class="text-danger">*</span>)
-                    </label>
-                    <input type="email" class="form-control" name="email" placeholder="name@example.com">
+                    <input name="email" type="email" class="form-control <?= $email_class_input ?? "" ?>" id="email" placeholder="Veuillez saisir votre adresse mail SVP !" value="<?= $_POST['email'] ?? "" ?>">
+
+                    <div class="<?= $email_class_feedback ?? "" ?> fw-bold">
+                        <?= $errors['email'] ?? "" ?>
+                    </div>
                 </div>
 
+                <div class="form-group mb-3">
+                    <label class="form-label" for="password">Mot de passe:</label>
 
-                <div class="mb-3">
-                    <label for="password" class="form-label">
-                        Mot de passe:
-                        (<span class="text-danger">*</span>)
-                    </label>
-                    <input type="password" class="form-control" name="password" placeholder="Mot de passe:">
+                    <input name="password" type="password" class="form-control <?= $password_class_input ?? "" ?>" id="password" name="password" placeholder="Veuillez saisir votre Mot de passe SVP !">
+
+                    <div class="<?= $password_class_feedback ?? "" ?> fw-bold">
+                        <?= $errors['password'] ?? "" ?>
+                    </div>
                 </div>
 
-                <div class="mb-3">
-                    <label for="password" class="form-label">
-                        Confirmer le mot de passe:
-                        (<span class="text-danger">*</span>)
-                    </label>
-                    <input type="password" class="form-control" name="password_confirme" placeholder=" Confirmer le mot de passe:">
+                <div class="form-group mb-3">
+                    <label class="form-label" for="password_confirm">Confirmer le mot de passe:</label>
+
+                    <input name="password_confirm" type="password" class="form-control <?= $password_confirm_class_input ?? "" ?>" id="password_confirm" name="password_confirm" placeholder="Veuillez confirmer le mot de passe SVP !">
+
+                    <div class="<?= $password_confirm_class_feedback ?? "" ?> fw-bold">
+                        <?= $errors['password_confirm'] ?? "" ?>
+                    </div>
                 </div>
+
 
                 <div class="mt-4">
 
-                    <button class="btn btn-dark">
+                    <button class="btn btn-dark" name="register_btn">
                         Créer un compte
                     </button>
 
